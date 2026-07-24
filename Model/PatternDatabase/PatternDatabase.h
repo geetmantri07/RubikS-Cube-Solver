e040381519d7439b26b18e7b542e49ef757b201d
Geet Mantri <geetmantri07@gmail.com>
2026-07-24 11:38:40 +0530

    Final Solver ready

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,19 @@
+//
+// Created by saket on 02-11-2023.
+//
+
+#ifndef RUBIKS_PATTERNDATABASE_H
+#define RUBIKS_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_PATTERNDATABASE_H
